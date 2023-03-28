962e81135aff51ac65e9461db2a6e0e2edb6a420
BalashovaMa <balashova.m.nd@gmail.com>
2023-03-28 13:25:43 +0300

    refactor hamburger menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeElement = document.querySelector('.menu__close');
+const overlay = document.querySelector('.menu__overlay');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -8,5 +9,9 @@ hamburger.addEventListener('click', () => {
 
 
 closeElement.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
     menu.classList.remove('active');
 });
