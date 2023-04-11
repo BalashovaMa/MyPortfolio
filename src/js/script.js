f473626726c9cf47ed5d24217568b5d3c16822fe
BalashovaMa <balashova.m.nd@gmail.com>
2023-04-11 13:37:38 +0300

    feat: add animation

--- src/js/script.js
+++ src/js/script.js
@@ -4,46 +4,45 @@ const closeElement = document.querySelector('.menu__close');
 const overlay = document.querySelector('.menu__overlay');
 
 hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
+  menu.classList.add('active');
 });
 
 
 closeElement.addEventListener('click', () => {
-    menu.classList.remove('active');
+  menu.classList.remove('active');
 });
 
 overlay.addEventListener('click', () => {
-    menu.classList.remove('active');
+  menu.classList.remove('active');
 });
 
 const btnUp = {
-    el: document.querySelector('.pageup'),
-    show() {
-      // удалим у кнопки класс btn-up_hide
-      this.el.classList.remove('pageup_hide');
-    },
-    hide() {
-      // добавим к кнопке класс btn-up_hide
-      this.el.classList.add('pageup_hide');
-    },
-    addEventListener() {
-      // при прокрутке содержимого страницы
-      window.addEventListener('scroll', () => {
-        // определяем величину прокрутки
-        const scrollY = window.scrollY || document.documentElement.scrollTop;
-        // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
-        scrollY > 400 ? this.show() : this.hide();
+  el: document.querySelector('.pageup'),
+  show() {
+    this.el.classList.remove('pageup_hide');
+  },
+  hide() {
+    this.el.classList.add('pageup_hide');
+  },
+  addEventListener() {
+
+    window.addEventListener('scroll', () => {
+      // определяем величину прокрутки
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      scrollY > 400 ? this.show() : this.hide();
+    });
+
+    document.querySelector('.pageup').onclick = () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
       });
-      // при нажатии на кнопку .btn-up
-      document.querySelector('.pageup').onclick = () => {
-        // переместим в начало страницы
-        window.scrollTo({
-          top: 0,
-          left: 0,
-          behavior: 'smooth'
-        });
-      }
     }
   }
-  
-  btnUp.addEventListener();
+}
+
+btnUp.addEventListener();
+
+new WOW().init();
+
